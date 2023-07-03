464c8c5e020364969c75224c49ff9d79972acfc5
asif.khan <asif.khan@gmail.com>
2023-07-03 16:12:48 +0530

    BLE code is added from ReactBle git project...............

--- index.js
+++ index.js
@@ -2,8 +2,30 @@
  * @format
  */
 
+// @flow
+
+export { BleError, BleErrorCode, BleAndroidErrorCode, BleIOSErrorCode, BleATTErrorCode } from './src/BleError'
+export { BleManager } from './src/BleManager'
+export { Device } from './src/Device'
+export { Service } from './src/Service'
+export { Characteristic } from './src/Characteristic'
+export { Descriptor } from './src/Descriptor'
+export { fullUUID } from './src/Utils'
+export { State, LogLevel, ConnectionPriority, ScanCallbackType, ScanMode } from './src/TypeDefinition'
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+export type {
+    Subscription,
+    DeviceId,
+    UUID,
+    TransactionId,
+    Base64,
+    ScanOptions,
+    ConnectionOptions,
+    BleManagerOptions,
+    BleRestoredState
+  } from './src/TypeDefinition'
 
 AppRegistry.registerComponent(appName, () => App);
